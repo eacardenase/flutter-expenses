00dbf4cf070259140f656b98ab1ad01178f1d017
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-06 09:23:06 -0500

    feat: add app bar and refactor expenses screen

--- lib/screens/expenses_screen.dart
+++ lib/screens/expenses_screen.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class ExpensesScreen extends StatelessWidget {
-  const ExpensesScreen({super.key});
+  ExpensesScreen({super.key});
+
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "NU",
+      amount: 1475900,
+      category: Category.work,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: "Udemy Courses",
+      amount: 59800,
+      category: Category.work,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Expenses(),
+        appBar: AppBar(
+          title: const Text(
+            "Expense Tracker",
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            )
+          ],
+        ),
+        body: Expenses(
+          expenses: _registeredExpenses,
+        ),
       ),
     );
   }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,38 +1,28 @@
-import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
-  const Expenses({super.key});
+  const Expenses({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
 
   @override
   State<Expenses> createState() => _ExpensesState();
 }
 
 class _ExpensesState extends State<Expenses> {
-  final List<Expense> _registeredExpenses = [
-    Expense(
-      title: "NU",
-      amount: 1475900,
-      category: Category.work,
-      date: DateTime.now(),
-    ),
-    Expense(
-      title: "Udemy Courses",
-      amount: 59800,
-      category: Category.work,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         const Text("Chart"),
         Expanded(
-          child: ExpensesList(expenses: _registeredExpenses),
+          child: ExpensesList(expenses: widget.expenses),
         ),
       ],
     );
