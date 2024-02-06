595e631b6fcb9a9bdb8a7debf49309375e3270c0
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-06 08:44:27 -0500

    feat: add expenses list and folder structure

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/widgets/expense_item.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
