be4fb0ce9e28f54d8d0368b48127d4a9532d7f80
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-07 19:16:17 -0500

    feat: setup main theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,38 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/screens/expenses_screen.dart';
 
+var colorScheme = ColorScheme.fromSeed(seedColor: Colors.cyan);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ExpensesScreen(),
+      theme: ThemeData().copyWith(
+        colorScheme: colorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: colorScheme.primaryContainer,
+          foregroundColor: colorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: colorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: colorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontStyle: FontStyle.italic,
+                // fontSize: 24,
+              ),
+            ),
+      ),
+      home: const ExpensesScreen(),
     ),
   );
 }
